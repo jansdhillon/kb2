c6ebadf4891d911b26f5c89bde738d7876533f2d
jansdhillon <77344313+jansdhillon@users.noreply.github.com>
2026-02-04 21:07:49 -0700

    init ufave/cli

--- cmd/kb2/main.go
+++ cmd/kb2/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v3"
+)
 
 func main() {
-	fmt.Printf("kb2\n")
+
+	ctx := context.Background()
+
+	cmd := &cli.Command{
+		Name:     "kb2",
+		Version:  "v0.0.1",
+		Usage:    "Root entry point for kb2",
+		Commands: []*cli.Command{},
+	}
+
+	if err := cmd.Run(ctx, os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jansdhillon/kb2
 
 go 1.25.6
+
+require github.com/urfave/cli/v3 v3.6.2 // indirect
